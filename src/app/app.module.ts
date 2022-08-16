bda52fec8cb2d1cd1a2a447d26fab25f9cefd924
dhiru1203 <dheerajpatel24499@gmail.com>
2022-08-16 13:07:36 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { HeaderComponent } from './header/header.component';
 import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent, HeaderComponent, DetailsComponent],
+  declarations: [AppComponent, ProductComponent, HeaderComponent, DetailsComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
