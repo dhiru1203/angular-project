a09b5e7293367a71aaeac6194b21cf9535fc61f6
dhiru1203 <dheerajpatel24499@gmail.com>
2022-08-16 15:33:34 +0530

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product/product.component';
 import { DetailsComponent } from './details/details.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path:'productList',component:ProductComponent},
-  {path:'productDetails/:id',component:DetailsComponent}
+  {path:'productDetails/:id',component:DetailsComponent},
+  {path:'home',component:HomeComponent}
 ];
 
 @NgModule({
